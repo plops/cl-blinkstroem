0fc88305a168abd8a38ae9908e4731eb6ef28dd2
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-11-21 00:41:31 +0100

    generate file

--- test.lisp
+++ test.lisp
@@ -8,7 +8,7 @@
 
 
 (defparameter *dat*
- (with-open-file (s "c:/Users/martin/Desktop/blinkstroem/BS2.BIN"
+ (with-open-file (s "BS2.BIN"
 		    :element-type `(unsigned-byte 8))
    (let* ((n (file-length s))
 	  (a (make-array n :element-type '(unsigned-byte 8))))
@@ -22,6 +22,12 @@
 	      (loop for j below (* 8 18) collect
 		   (aref *dat* (+ i 2 j)))))))
 
+#+nil
+(loop for (tim frame) in *imgs* maximize
+     (loop for v in frame maximize v))
+;; max value is 11
+
+
 (defparameter *vid*
  (loop for (tim img) in *imgs* collect
       (let* ((h 8)
@@ -29,7 +35,8 @@
 	     (im (make-array (list w h) :element-type '(unsigned-byte 8)))
 	     (im1 (sb-ext:array-storage-vector im)))
 	(dotimes (i (length im1))
-	  (setf (aref im1 i) (elt img i)))
+	  (setf (aref im1 i) (floor (* 255 (elt img i))
+				    11)))
 	im)))
 #+nil
 (length *vid*)
@@ -45,3 +52,56 @@
 	      (format s "~2d" (aref frame j i)))
 	    (terpri s)))
 	(terpri s))))
+
+(defun write-pgm (filename img)
+  (declare (simple-string filename)
+           ((array (unsigned-byte 8) 2) img)
+           (values null &optional))
+  (destructuring-bind (h w)
+      (array-dimensions img)
+    (declare ((integer 0 65535) w h))
+    (with-open-file (s filename
+                       :direction :output
+                       :if-exists :supersede
+                       :if-does-not-exist :create)
+      (declare (stream s))
+      (format s "P5~%~D ~D~%255~%" w h))
+    (with-open-file (s filename 
+                       :element-type '(unsigned-byte 8)
+                       :direction :output
+                       :if-exists :append)
+      (let ((data-1d (make-array 
+                      (* h w)
+                      :element-type '(unsigned-byte 8)
+                      :displaced-to img)))
+        (write-sequence data-1d s)))
+    nil))
+
+(loop for k below (length *vid*) do
+     (let ((frame (elt *vid* k)))
+       (write-pgm (format nil "/dev/shm/f~6,'0d.pgm" k)
+		  frame)))
+
+
+(defparameter *balken*
+ (let* ((h 8)
+	(w 18)
+	(n 180)
+	(a (make-array (list n h w) :element-type '(unsigned-byte 8))))
+   (loop for k below n do
+	(loop for j below h do
+	     (loop for i below w do
+		  (setf (aref a k j i) (if (= i (mod k 18)) 11 0)))))
+   a))
+
+(with-open-file (s "/dev/shm/o.bin" :direction :output
+		   :if-exists :supersede
+		   :if-does-not-exist :create
+		   :element-type '(unsigned-byte 8))
+  (destructuring-bind (n h w) (array-dimensions *balken*)
+    (loop for k below n do
+	 (write-byte 64 s)
+	 (write-byte 0 s)
+	 (loop for j below h do
+	      (loop for i below w do
+		   (write-byte (aref *balken* k j i) s))))))
